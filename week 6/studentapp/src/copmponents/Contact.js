424bf9b55b23852a87b852de6f59fcffc6e68cf9
Vigneshtutorials <155253878+Vigneshtutorials@users.noreply.github.com>
2025-07-27 13:55:58 +0530

    Add files via upload

---
+++ week 6/studentapp/src/copmponents/Contact.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <h1>Welcome to the Contact Page of the Student Management Portal.</h1>
+    );
+  }
+}
+export default Contact;
