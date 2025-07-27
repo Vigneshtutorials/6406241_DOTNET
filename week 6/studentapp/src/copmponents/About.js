298805bd9e38e129f5ea6171bd1f9cbb4445dc12
Vigneshtutorials <155253878+Vigneshtutorials@users.noreply.github.com>
2025-07-27 14:02:43 +0530

    Delete week 6 directory

--- week 6/studentapp/src/copmponents/About.js
+++
@@ -1,8 +0,0 @@
-import { Component } from "react";
-
-class About extends Component {
-  render() {
-    return <h1>Welcome to the About Page of the Student Management Portal.</h1>;
-  }
-}
-export default About;
