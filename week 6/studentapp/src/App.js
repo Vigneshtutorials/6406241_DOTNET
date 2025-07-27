151f48e1187de905803f33743f3c2b8ee345e3ce
Vigneshtutorials <155253878+Vigneshtutorials@users.noreply.github.com>
2025-07-27 14:08:50 +0530

    Add files via upload

---
+++ week 6/studentapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import About from './copmponents/About';
+import Contact from './copmponents/Contact';
+import Home from './copmponents/Home';
+function App() {
+  return (
+    <div className="App">
+     <Home />
+     <About />
+     <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ week 6/studentapp/src/copmponents/About.js
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+class About extends Component {
+  render() {
+    return <h1>Welcome to the About Page of the Student Management Portal.</h1>;
+  }
+}
+export default About;
